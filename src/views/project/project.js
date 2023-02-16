f548936ce04cfe71924838bb35e17aff02970e12
Yasir Khan <yasir_electronics@yahoo.com>
2023-02-17 00:41:18 +0100

    fix the video paths
    Signed-off-by: Yasir Khan <yasir_electronics@yahoo.com>

--- src/views/project/project.js
+++ src/views/project/project.js
@@ -2,12 +2,12 @@ import React from "react";
 import "./project.css"
 import projects from "./projects/projects.json"
 
-import Video1 from "./resources/1.mp4";
-import Video2 from "./resources/2.mp4";
-import Video3 from "./resources/3.mp4";
-import Video4 from "./resources/4.mp4";
-import Video5 from "./resources/5.mp4";
-import Video6 from "./resources/6.mp4";
+// import Video1 from "./resources/1.mp4";
+// import Video2 from "./resources/2.mp4";
+// import Video3 from "./resources/3.mp4";
+// import Video4 from "./resources/4.mp4";
+// import Video5 from "./resources/5.mp4";
+// import Video6 from "./resources/6.mp4";
 
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 import {
@@ -16,7 +16,7 @@ import {
     useParams,
 } from "react-router-dom";
 
-let videos = [Video1, Video2, Video3, Video4, Video5, Video6]
+// let videos = [Video1, Video2, Video3, Video4, Video5, Video6]
 
 function withRouter(Component) {
     function ComponentWithRouterProp(props) {
@@ -53,19 +53,19 @@ class Project extends React.Component {
 
     render() {
         let id = this.props.router.params.id;
+
         console.log(projects[id])
-        let { title, width = "50%", description } = projects[id];
-
-        console.log('title' + title)
-
+        let { title, video, description } = projects[id];
+        let videoPath = process.env.PUBLIC_URL + video;
+        console.log(videoPath)
         return <TransitionGroup component={null}>
             <CSSTransition classNames="project" timeout={0} in={this.state.mounted} appear>
                 <div className="project-container">
                     <div className='project-title'>
                         {title}
                     </div>
-                    < div className='project-video' style={{ width }}>
-                        <video src={videos[id]} controls loop autoplay />
+                    < div className='project-video'>
+                        <video src={videoPath} controls loop autoplay />
                         {/* <iframe src={videos[id]} frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe> */}
                     </div>
                     <div className="project-description">{description}</div>
